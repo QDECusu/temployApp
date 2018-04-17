dc3e329fe5f353895bec1a856e22ca37246f6cd5
Dax <graydax@gmail.com>
2018-04-16 22:06:41 -0600

    y

--- src/components/screens/EditJobPost.js
+++ src/components/screens/EditJobPost.js
@@ -6,51 +6,53 @@ import { Input, Button, colors } from "../utils";
 import { request } from "../../api";
 import { getMyJobs } from "../../actions/jobs";
 import JobPosting from "../JobPosting";
-import { Divider, Avatar } from 'react-native-elements';
+import { Divider, Avatar } from "react-native-elements";
 
 const mapStateToProps = ({ profile, myJobs }) => ({ profile, myJobs });
 
 const mapDispatchToProps = { getMyJobs };
 
 @connect(mapStateToProps, mapDispatchToProps)
-class Profile extends Component {
-  static navigationOptions = ({ navigation }) => ({ title: navigation.state.params.title })
+class EditJobPost extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.state.params.title
+  });
   componentWillMount() {
     this.props.getMyJobs();
   }
 
-  editJobPost = () => {
-      this.props.navigation.navigate("EditJobPost")
-  }
-
-  editProfile = () => {
-    this.props.navigation.navigate("EditProfile");
-  };
-
-  logout = () => {
-    request.logout();
-    this.props.navigation.navigate("Authentication");
-  };
-
   render() {
     const { profile, myJobs } = this.props;
     return (
-      <ScrollView style={{backgroundColor: "skyblue"}}>
-        <View style={{ 
+      <ScrollView style={{ backgroundColor: "skyblue" }}>
+        <View
+          style={{
             flex: 1,
             justifyContent: "center",
             alignItems: "center",
             backgroundColor: "skyblue",
             marginBottom: 30
-          }}>
-          <View style={{alignItems:"center"}}>
-          <Button label="Edit Job Post" onPress={this.editJobPost} />
-          <Button label="Save" onPress={this.logout} />
-            <Divider style={{ margin: 20, width: 300, backgroundColor: colors.primaryDeep }} />          
+          }}
+        >
+          <View style={{ alignItems: "center" }}>
+            <Button label="Edit Job Post" onPress={this.editJobPost} />
+            <Button label="Save" onPress={this.logout} />
+            <Divider
+              style={{
+                margin: 20,
+                width: 300,
+                backgroundColor: colors.primaryDeep
+              }}
+            />
           </View>
           <Text style={style}>MY JOB POSTS</Text>
           {myJobs.map(jp => (
-            <JobPosting own key={jp.company_name} jobPosting={jp} onPress={this.editJobPost}/>
+            <JobPosting
+              own
+              key={jp.company_name}
+              jobPosting={jp}
+              onPress={this.editJobPost}
+            />
           ))}
         </View>
       </ScrollView>
@@ -63,7 +65,7 @@ const style = {
   fontSize: 20,
   color: "white",
   alignItems: "center",
-  justifyContent: "center",
+  justifyContent: "center"
 };
 
-export default Profile;
+export default EditJobPost;
--- src/components/screens/EditProfile.js
+++ src/components/screens/EditProfile.js
@@ -6,19 +6,19 @@ const mapStateToProps = ({ profile }) => ({ profile });
 
 @connect(mapStateToProps)
 class EditProfile extends Component {
-    state = {
-      user: {
-        email: this.props.profile.email,
-        first_name: this.props.profile.first_name,
-        last_name: this.props.profile.last_name
-      },
-      zipcode: "",
-      short_description: "",
-      skills: ""
-    };
+  state = {
+    user: {
+      email: this.props.profile.email,
+      first_name: this.props.profile.first_name,
+      last_name: this.props.profile.last_name
+    },
+    zipcode: "",
+    short_description: "",
+    skills: ""
+  };
   render() {
     return <View />;
   }
 }
 
-export default EditProfile
+export default EditProfile;
